7d7c4c86fa9c8e8d74d20229403ae97952a219a0
Nes <epmjj4@hotmail.com>
2021-03-31 13:52:47 -0400

    server is listening

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes';)
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+// configurations for static, and routes middleware
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+// set up server listener
+
+app.listen(PORT, () => {
+    console.log("Server is listening on PORT: ${port");
+})
+
 
